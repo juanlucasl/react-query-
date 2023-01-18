bfef12e3696a7c15c767baf588c0e07eefc22163
Juan L <33398520+juanlucasl@users.noreply.github.com>
2023-01-17 22:46:56 -0300

    Test treatments query response

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,6 +1,20 @@
+import { screen } from '@testing-library/react';
+
 import { renderWithQueryClient } from '../../../test-utils';
 import { Treatments } from '../Treatments';
 
-test('renders response from query', () => {
-  renderWithQueryClient(<Treatments />);
+describe('Treatments', () => {
+  test('renders without error', () => {
+    renderWithQueryClient(<Treatments />);
+  });
+
+  test('renders response from query', async () => {
+    renderWithQueryClient(<Treatments />);
+
+    const treatmentTitles = await screen.findAllByRole('heading', {
+      name: /massage|facial|scrub/i,
+    });
+
+    expect(treatmentTitles).toHaveLength(3);
+  });
 });
