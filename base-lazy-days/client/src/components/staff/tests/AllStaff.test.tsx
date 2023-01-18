fd8f4bd6bd9f441a07186c8e1bd65d5358e4b013
Juan L <33398520+juanlucasl@users.noreply.github.com>
2023-01-17 22:52:23 -0300

    Test staff query response

--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,20 +1,33 @@
 import { screen } from '@testing-library/react';
 
+import { renderWithQueryClient } from '../../../test-utils';
 // import { rest } from 'msw';
 // import { defaultQueryClientOptions } from '../../../react-query/queryClient';
 // import { server } from '../../../mocks/server';
 // import { renderWithClient } from '../../../test-utils';
 import { AllStaff } from '../AllStaff';
 
-test('renders response from query', () => {
-  // write test here
-});
+describe('All Staff', () => {
+  test('renders without error', () => {
+    renderWithQueryClient(<AllStaff />);
+  });
 
-test('handles query error', async () => {
-  // (re)set handler to return a 500 error for staff
-  // server.resetHandlers(
-  //   rest.get('http://localhost:3030/staff', (req, res, ctx) => {
-  //     return res(ctx.status(500));
-  //   }),
-  // );
+  test('renders response from query', async () => {
+    renderWithQueryClient(<AllStaff />);
+
+    const staffNAmes = await screen.findAllByRole('heading', {
+      name: /sandra|divya|mateo|michael/i,
+    });
+
+    expect(staffNAmes).toHaveLength(4);
+  });
+
+  test('handles query error', async () => {
+    // (re)set handler to return a 500 error for staff
+    // server.resetHandlers(
+    //   rest.get('http://localhost:3030/staff', (req, res, ctx) => {
+    //     return res(ctx.status(500));
+    //   }),
+    // );
+  });
 });
