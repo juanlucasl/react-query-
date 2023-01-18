3852ee644c82e96f8f5e59a906e4faa52cbc09e3
Juan L <33398520+juanlucasl@users.noreply.github.com>
2023-01-17 23:47:23 -0300

    Test useAppointments hook

--- base-lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
+++ base-lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
@@ -1,8 +1,42 @@
-// import { act, renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
 
-// import { createWrapper } from '../../../test-utils';
+import { createQueryClientWrapper } from '../../../test-utils';
 import { useAppointments } from '../hooks/useAppointments';
+import { AppointmentDateMap } from '../types';
 
-test('filter appointments by availability', async () => {
-  // test goes here
+/**
+ * Returns the total number of appointments on an AppointmentDateMap object.
+ * @param appointments
+ */
+const getAppointmentCount = (appointments: AppointmentDateMap) =>
+  Object.values(appointments).reduce(
+    (runningCount, appointmentsOnDate) =>
+      runningCount + appointmentsOnDate.length,
+    0,
+  );
+
+describe('useAppointments hook', () => {
+  test('filter appointments by availability', async () => {
+    const { result, waitFor } = renderHook(useAppointments, {
+      wrapper: createQueryClientWrapper(),
+    });
+
+    // Wait for the appointments to populate.
+    await waitFor(() => getAppointmentCount(result.current.appointments) > 0);
+
+    const filteredAppointmentLength = getAppointmentCount(
+      result.current.appointments,
+    );
+
+    // Set to filter to all appointments.
+    act(() => result.current.setShowAll(true));
+
+    // Wait for the appointments to show more than when filtered.
+    await waitFor(() => {
+      return (
+        getAppointmentCount(result.current.appointments) >
+        filteredAppointmentLength
+      );
+    });
+  });
 });
